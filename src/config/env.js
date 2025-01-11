88761a2109e43307324a89f0676d8f9523dae54b
Soufiane ELBOUBKARI <elboubkarisoufiane03@gmail.com>
2025-01-11 17:26:03 +0100

    Les réponses aux questions de env.js

--- src/config/env.js
+++ src/config/env.js
@@ -1,7 +1,7 @@
 // Question: Pourquoi est-il important de valider les variables d'environnement au démarrage ?
-// Réponse : 
+// Réponse : Pour s'assurer que toutes les configurations nécessaires sont présentes et correctes avant que l'application ne commence à fonctionner. Cela permet d'éviter des erreurs inattendues en cours d'exécution et de garantir que l'application dispose de toutes les informations nécessaires pour se connecter aux services externes, comme les bases de données.
 // Question: Que se passe-t-il si une variable requise est manquante ?
-// Réponse : 
+// Réponse : L'application doit lever une erreur explicative et arrêter son exécution. Cela permet de signaler immédiatement le problème de configuration et d'éviter des comportements imprévisibles ou des échecs plus difficiles à diagnostiquer plus tard.
 
 const dotenv = require('dotenv');
 dotenv.config();
