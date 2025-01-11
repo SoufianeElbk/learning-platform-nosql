b1e7ca0ffa2cd6ef779a92f4f6181deb99b049d9
Soufiane ELBOUBKARI <elboubkarisoufiane03@gmail.com>
2025-01-11 17:41:53 +0100

    Les réponses aux questions de courseController.js

--- src/controllers/courseController.js
+++ src/controllers/courseController.js
@@ -1,7 +1,7 @@
 // Question: Quelle est la différence entre un contrôleur et une route ?
-// Réponse:
+// Réponse: Une route définit les points d'entrée de l'application et associe les requêtes HTTP à des fonctions spécifiques. Un contrôleur, en revanche, contient la logique métier qui est exécutée lorsque ces points d'entrée sont atteints. Les routes dirigent les requêtes vers les contrôleurs appropriés.
 // Question : Pourquoi séparer la logique métier des routes ?
-// Réponse :
+// Réponse : Séparer la logique métier des routes permet de maintenir un code propre et organisé. Cela facilite la maintenance, les tests et la réutilisation du code. En isolant la logique métier dans des contrôleurs, on peut également mieux structurer l'application et suivre le principe de séparation des préoccupations.
 
 const { ObjectId } = require('mongodb');
 const db = require('../config/db');
