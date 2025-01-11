5d7b73c7080e12db1e71eb24a80798916e128534
Soufiane ELBOUBKARI <elboubkarisoufiane03@gmail.com>
2025-01-11 17:32:18 +0100

    Les réponses aux questions de mongoService.js

--- src/services/mongoService.js
+++ src/services/mongoService.js
@@ -1,5 +1,5 @@
 // Question: Pourquoi créer des services séparés ?
-// Réponse: 
+// Réponse: Créer des services séparés permet de centraliser la logique métier et les interactions avec les bases de données ou autres services externes. Cela rend le code plus modulaire, réutilisable et maintenable. En séparant les services, on peut également faciliter les tests unitaires et l'évolution du code sans impacter les autres parties de l'application.
 
 const { ObjectId } = require('mongodb');
 
